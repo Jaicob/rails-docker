8c9df603af631aa1e4c9a88f29cf214012ca7fed
Jaicob <jaicob@icloud.com>
2015-06-18 13:40:13 -0400

    add sample project with unicorn

---
+++ rails-nginx-unicorn/app_entrypoint.sh
@@ -0,0 +1,27 @@
+RAILS_ENV=$RAILS_ENV
+: ${RAILS_ENV:="development"}
+export RAILS_ENV
+
+SECRET_KEY_BASE=$SECRET_KEY_BASE
+: ${SECRET_KEY_BASE:="f38c575fcf0b2a0e7c7f002a873d54d78104581ebe069bf2b1afad04014d1e10245b259b872b0e12189ef2ce3fca4c73a9b5103aaf4aad1f4"}
+export SECRET_KEY_BASE=$SECRET_KEY_BASE
+
+## Setting DB
+DB_NAME="my_app_${RAILS_ENV}"
+#DATABASE_URL="mysql2://root:root@localhost/${DB_NAME}"
+
+# Trap sigkill and sigterm: otherwise dockr stop/start will complain for stale unicorn pid
+trap "pkill unicorn_rails ; exit " SIGINT SIGTERM SIGKILL
+
+echo "Stopping  unicorn_rails, if already running"
+pkill unicorn_rails
+
+echo "cleaning tmp files"
+rm -rf /home/rails/my-app/tmp/*
+
+echo "Restart Reverse Proxy"
+sudo service nginx reload && sudo service nginx restart
+
+echo "Running unicorn"
+sudo service unicorn start
+#bundle exec unicorn_rails -c /etc/web-app/unicorn.rb -E $RAILS_ENV -d
